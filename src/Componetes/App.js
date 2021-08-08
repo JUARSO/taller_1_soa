d077f1593b88b81cceecd5a9dc7231c310f29e7b
JUARSO <juan09fut.js@gmail.com>
2021-08-07 21:58:28 -0600

    aA

---
+++ src/Componetes/App.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import './App.css';
+import React from "react";
+import Layaut from "./Layaut";
+import Tabla from "./Tabla";
+function App() {
+  return (
+    <div className="App">
+      <Layaut/>
+    </div>
+  );
+}
+
+export default App;
